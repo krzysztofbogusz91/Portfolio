1cf3430501b2836f66acc440df468f017fd9ddd7
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-03-03 16:33:06 +0100

    add_mobile_styles

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,9 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const arrowLeft = document.getElementById('arrow-left');
     const projectsGallery = document.getElementById('projects-gallery');
     const circNav = document.getElementById('circles').children;
-
+    const mobileNav = document.getElementById('mobile-nav');
+    const menu = document.getElementById('menu')
     const gallery = projectsGallery.children;
 
+
+    //MOBILE NAV
+    mobileNav.addEventListener('click', function () {
+        menu.classList.toggle('show-nav');
+    })
+
+
+    //SLIDER
     let counter = 0;
     arrowRight.addEventListener('click', function () {
         if (counter >= gallery.length-1 || counter < 0) {
