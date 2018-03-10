84227a5037f42814cd0b022b559035e27490ed28
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-03-10 20:40:04 +0100

    js_start_form_add_left_arrow

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //SLIDER AND NAV
     const arrowRight = document.getElementById('arrow-right');
     const arrowLeft = document.getElementById('arrow-left');
     const projectsGallery = document.getElementById('projects-gallery');
@@ -6,7 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const mobileNav = document.getElementById('mobile-nav');
     const menu = document.getElementById('menu')
     const gallery = projectsGallery.children;
+    //FORM VALID
+    const formSend = document.getElementById('form-send');
 
+    //FORM VALIDATION
+    // formSend.addEventListener('click', function () {
+    //    // e.preventDefault();
+    //     console.log('daf');
+    //
+    //     alert('form is disabled');
+    // });
 
     //MOBILE NAV
     mobileNav.addEventListener('click', function () {
@@ -22,7 +32,24 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             counter++;
         }
-        console.log(counter);
+
+        for (let i = 0; i < gallery.length; i++) {
+            gallery[i].classList.remove('show');
+            circNav[i].classList.remove('active');
+            if (i === counter) {
+                gallery[i].classList.add('show');
+                circNav[i].classList.add('active');
+            }
+        }
+
+    });
+
+    arrowLeft.addEventListener('click', function () {
+        counter--;
+        if(counter < 0){
+            counter = 1;
+        }
+
         for (let i = 0; i < gallery.length; i++) {
             gallery[i].classList.remove('show');
             circNav[i].classList.remove('active');
